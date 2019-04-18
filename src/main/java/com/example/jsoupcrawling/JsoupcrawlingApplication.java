8b5ecf223f8123097299badbffbbfcf9796416dc
ben <lhy880518@naver.com>
2019-04-18 17:00:01 +0900

    master #comment War배포를 위한 설정 추가

--- src/main/java/com/example/jsoupcrawling/JsoupcrawlingApplication.java
+++ src/main/java/com/example/jsoupcrawling/JsoupcrawlingApplication.java
@@ -2,11 +2,18 @@ package com.example.jsoupcrawling;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-public class JsoupcrawlingApplication {
+public class JsoupcrawlingApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(JsoupcrawlingApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(JsoupcrawlingApplication.class, args);
